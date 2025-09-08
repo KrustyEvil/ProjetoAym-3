0bd1e0ce13c83c3e744322208dd7c7efd187affa
José Carlos <93680700+KrustyEvil@users.noreply.github.com>
2025-09-07 21:13:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Projeto_Pedido_Namoro_Avan-ado/',
+  base: '/ProjetoAym-3/',
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
